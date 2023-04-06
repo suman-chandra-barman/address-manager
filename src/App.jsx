178b59dfacbc89a81466af46e3f30ae9eaf8703c
Suman-Chandra-Barman <sumandranathbarman@gmail.com>
2023-04-06 21:41:21 +0600

    tailwind install and file structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
